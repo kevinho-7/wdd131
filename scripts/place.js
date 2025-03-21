e9ec53c8fbe785a64969f097e8b9330a9f5bfbea
kevinho-7 <kevingarciaf.kf@outlook.com>
2025-03-20 22:58:14 -0300

    its problably done

---
+++ scripts/place.js
@@ -0,0 +1,23 @@
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
+
+
+function calculateWindChill(temperature, windSpeed) {
+    return (13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16)).toFixed(1);
+}
+
+const temperature = 18; 
+const windSpeed = 5; 
+
+let windChill = "N/A";
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill = calculateWindChill(temperature, windSpeed) + " °C";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const windChillElement = document.querySelector(".weather-item span#value4");
+    if (windChillElement) {
+        windChillElement.textContent = windChill;
+    }
+});
